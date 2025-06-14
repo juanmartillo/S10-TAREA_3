b2719a079ad743a82266b31f8ad11da1a291fdae
Juan Martillo Moran <112291526+juanmartillo@users.noreply.github.com>
2025-06-14 15:29:42 -0500

    Update config.js

--- Backend/dist/config.js
+++ Backend/dist/config.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.PORT = exports.MongoUrl = void 0;
-exports.MongoUrl = "mongodb+srv://RodyMacay:RodyMacay2003@cluster0.qwkvvov.mongodb.net/";
+exports.MongoUrl = "";
 exports.PORT = 5000;
